c509c5f811a881fd744c00c7c3cfe6f17fe00bd0
Anders Svensson <anders.m.sv@gmail.com>
2018-01-03 15:15:43 +0100

    Pseudo-code and styles added

--- scripts/game.js
+++ scripts/game.js
@@ -1,16 +1,32 @@
 var numGuesses = 0;
 var question = "";
+var initAnimal = "cat";
+
 var messageDisplay = document.querySelector("#questionDisplay");
-var initQuestions = ["Is it a cat?", "Is it a dog?", "Is it a fish?"];
+var buttons = document.querySelectorAll("button");
+var yesBtn = document.querySelector("#yesBtn");
+var noBtn = document.querySelector("#noBtn");
 
 init();
 
 function init() {
-    var randomItem = initQuestions[Math.floor(Math.random()*initQuestions.length)];
-    question = randomItem;
+    question = `Are you thinking of a ${initAnimal}?`;
     messageDisplay.innerHTML = question;
 }
 
-if (numGuesses >= 20) {
-    question = "What animal did you think of?";
+function checkAnswer(answer) {
+    //funktion som kollar om spelaren svarat ja eller nej
+}
+
+function checkNumGuesses(numGuesses) {
+    //kontrollera hur många frågor som ställts
+    if (numGuesses >= 20) {
+        //ändra frågan och låt spelaren skriva in djuret
+        question = `What animal did you think of?`;
+        buttons.style.classList = "inactive";
+    }
+}
+
+function newAnimal(playerInput) {
+    //ta spelarens input och skapa ett nytt djur
 }
