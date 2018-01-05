45071fa6fe53b309357046aff25de11cfd7589da
Anders Svensson <anders.m.sv@gmail.com>
2018-01-05 11:56:17 +0100

    Minor changes and comments

--- scripts/game.js
+++ scripts/game.js
@@ -5,27 +5,41 @@ var initAnimal = "cat";
 var messageDisplay = document.querySelector("#questionDisplay");
 var yesBtn = document.querySelector("#yesBtn");
 var noBtn = document.querySelector("#noBtn");
+var animalInput = document.querySelector("#inputAnimal");
 
 init();
 
 function init() {
-    question = `Are you thinking of a ${initAnimal}?`;
-    messageDisplay.innerHTML = question;
+    messageDisplay.innerHTML = `Are you thinking of a ${initAnimal}?`;
+
+    noBtn.addEventListener("click", function(){
+        numGuesses++;
+        checkNumGuesses(numGuesses);
+    });
+
+    animalInput.addEventListener('invalid', function(e){
+        if(animalInput.validity.valueMissing){
+            e.target.setCustomValidity("Hey, gimme something to work with here!");
+        }
+    });
 }
 
 function checkAnswer(answer) {
-    //funktion som kollar om spelaren svarat ja eller nej
+    //check if player answered 'yes' or 'no'
 }
 
 function checkNumGuesses(numGuesses) {
-    //kontrollera hur många frågor som ställts
-    if (numGuesses >= 20) {
-        //ändra frågan och låt spelaren skriva in djuret
+    //check the number of questions asked
+    if (numGuesses >= 5) {
+        //change question, remove buttons and show input field
         question = `What animal did you think of?`;
-        buttons.style.classList = "inactive";
+        messageDisplay.innerHTML = question;
+        yesBtn.classList.toggle("inactive");
+        noBtn.classList.toggle("inactive");
+        animalInput.classList.toggle("inactive");
     }
 }
 
 function newAnimal(playerInput) {
-    //ta spelarens input och skapa ett nytt djur
+    //use player input to create a new animal
 }
