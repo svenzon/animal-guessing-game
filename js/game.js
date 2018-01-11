47cbd8e2c579c88e28c8dc539bf3575b7025b263
Anders Svensson <anders.m.sv@gmail.com>
2018-01-11 15:41:07 +0100

    Working on JSON

--- js/game.js
+++ js/game.js
@@ -2,22 +2,41 @@ var numGuesses = 0;
 var question = "";
 var initAnimal = "cat";
 var initQuestion = `Are you thinking of a ${initAnimal}?`;
-var userInput = '';
+var userInput = "";
 
 var messageDisplay = document.querySelector("#questionDisplay");
 var yesBtn = document.querySelector("#yesBtn");
 var noBtn = document.querySelector("#noBtn");
 var animalInput = document.querySelector("#inputAnimal");
 
+var requestURL = 'https://github.com/svenzon/animal-guessing-game/blob/master/json/animals.json';
+
 //self-invoking function which adds necessary eventlisteners
 (function init () {
-    messageDisplay.innerHTML = initQuestion;
+    yesBtn.addEventListener("click", function(){
+        numGuesses++;
+        checkNumGuesses(numGuesses);
+    });
 
     noBtn.addEventListener("click", function (){
         numGuesses++;
         checkNumGuesses(numGuesses);
     });
+
+    //getJSON();
 }());
+
+function getJSON() {
+    let request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.responseType = 'json';
+    request.send();
+
+    request.onload = function() {
+        let animals = request.response;
+        console.log(animals);
+    }
+}
 
 function checkAnswer(answer) {
     //check if player answered 'yes' or 'no'
@@ -27,6 +46,7 @@ function checkNumGuesses(numGuesses) {
     //check the number of questions asked
     if (numGuesses >= 5) {
         //change question, remove buttons and show input field
+        //note: move to separate function
         question = `What animal did you think of?`;
         messageDisplay.innerHTML = question;
         yesBtn.classList.toggle("inactive");
@@ -38,5 +58,4 @@ function checkNumGuesses(numGuesses) {
 function newAnimal() {
     //use player input to create a new animal
     userInput = animalInput.value;
-    alert(userInput);
 }
