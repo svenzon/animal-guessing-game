479c210b482e495d8ce0572dd61c5e239835e9e8
Anders Svensson <anders.m.sv@gmail.com>
2018-01-19 13:37:42 +0100

    DB connection script moved

--- buildScripts/server.js
+++ buildScripts/server.js
@@ -3,14 +3,12 @@ import path from "path";
 import open from "open";
 import webpack from "webpack";
 import config from "../webpack.config.dev";
-import MongoClient from "mongodb";
 import chalk from "chalk";
+import * as dbModule from "../db/db";
 
 const port = 3000;
 const app = express();
 const compiler = webpack(config);
-const dbURI = "mongodb://localhost:27017/animal_app";
-const dbCollection = "animals";
 
 app.use(require("webpack-dev-middleware")(compiler, {
     noInfo: true,
@@ -26,21 +24,6 @@ app.listen(port, function(err) {
         console.log(err);
     } else {
         open("http://localhost:" + port);
-    }
-});
-
-MongoClient.connect(dbURI, (err, client) => {
-    if (!err) {
-        var db = client.db("animal_app");
-        console.log(chalk.blue("Connected!"));
-        db.collection(dbCollection, function (err, returnCollection) {
-            returnCollection.find({ "feature": "carnivorous" }).project({ name: 1, _id: 0}).toArray(function (err, items){
-                items.forEach(function(element){
-                    console.log(element);
-                });
-            });
-        });
-    } else {
-        console.log(chalk.red("Oh no!"));
+        dbModule.connectToDB();
     }
 });
--- db/db.js
+++ db/db.js
@@ -5,19 +5,29 @@
 // aquatic: Boolean,
 // size: String,
 // feature: String
+import MongoClient from "mongodb";
+import chalk from "chalk";
 
-// MongoClient.connect(dbURI, (err, client) => {
-//     if (!err) {
-//         var db = client.db("animal_app");
-//         console.log(chalk.blue("Connected!"));
-//         db.collection(dbCollection, function (err, returnCollection) {
-//             returnCollection.find({ "feature": "carnivorous" }).project({ name: 1, _id: 0}).toArray(function (err, items){
-//                 items.forEach(function(element){
-//                     console.log(element);
-//                 });
-//             });
-//         });
-//     } else {
-//         console.log(chalk.red("Oh no!"));
-//     }
-// });
+const dbURI = "mongodb://localhost:27017/animal_app";
+const dbCollection = "animals";
+
+module.exports = {
+    
+    connectToDB: function () {
+        MongoClient.connect(dbURI, (err, client) => {
+            if (!err) {
+                var db = client.db("animal_app");
+                console.log(chalk.blue("Connected!"));
+                db.collection(dbCollection, function (err, returnCollection) {
+                    returnCollection.find({ "feature": "carnivorous" }).project({ name: 1, _id: 0}).toArray(function (err, items){
+                        items.forEach(function(element){
+                            console.log(element);
+                        });
+                    });
+                });
+            } else {
+                console.log(chalk.red("Oh no!"));
+            }
+        });
+    }
+}
